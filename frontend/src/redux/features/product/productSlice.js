3fc024ed30c033355f8a2b7cfaafa4e0875fac1c
Vaibhav <85175075+vibhu2000@users.noreply.github.com>
2024-10-16 16:22:36 +0530

    dashboard -> total store value count changed to selling price

--- frontend/src/redux/features/product/productSlice.js
+++ frontend/src/redux/features/product/productSlice.js
@@ -116,8 +116,8 @@ const productSlice = createSlice({
       const products = action.payload;
       const array = [];
       products.map((item) => {
-        const { price, quantity } = item;
-        const productValue = price * quantity;
+        const { selling_price, quantity } = item;
+        const productValue = selling_price * quantity;
         return array.push(productValue);
       });
       const totalValue = array.reduce((a, b) => {
